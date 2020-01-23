b8bfa1d8205dd9bc315367e99be2edccb535fd2f
KamiFightingSpirit <55668557+KamiFightingSpirit@users.noreply.github.com>
2020-01-23 13:45:38 +0100

    Add Unstyled Slider
    used slider library, npm install --save rc-slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import Palette from "./Components/Palette.js";
 import seedColors from "./Components/seedColors.js";
+import { generatePalette } from "./Components/colorHelpers.js";
 
 class App extends Component {
   render() {
-    return <Palette {...seedColors[4]} />;
+    return (
+      <div>
+        <Palette palette={generatePalette(seedColors[4])} />;
+      </div>
+    );
   }
 }
 
